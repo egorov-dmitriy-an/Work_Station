68d03c3fb7bd70f188106192f17ce5f3dc2b133a
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-03-31 13:05:12 +0300

    Add Seminar 5 Task 36

---
+++ ConsoleApp047 Seminar 5 Task 34/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив, заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// Например: [345, 897, 568, 234] -> 2.
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("================ Семинар 5 Задача 34 ================");
+            int Quant(int len)
+            {
+                int[] array_3zn = new int[len];
+                int quantity = 0;
+                for (int i = 0; i < len; i++)
+                {
+                    array_3zn[i] = new Random().Next(100, 1000);
+                    Console.Write(array_3zn[i] + ", ");
+                    if (array_3zn[i] % 2 == 0) quantity += 1;
+                }
+                Console.WriteLine("");
+                return quantity;
+            }
+            Console.Write("Введите размер массива: ");
+            int len2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Количество четных элементов в массиве составляет: {Quant(len2)}");
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ ConsoleApp048 Seminar 5 Task 36/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях. Например:
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("================ Семинар 5 Задача 36 ================");
+            int Summa(int len)
+            {
+                int[] array_rand = new int[len];
+                int sum = 0;
+                for (int i = 0; i < len; i++)
+                {
+                    array_rand[i] = new Random().Next(-999, 1000);
+                    Console.Write(array_rand[i] + ", ");
+                    if (i % 2 != 0) sum = sum + array_rand[i];
+                }
+                Console.WriteLine("");
+                return sum;
+            }
+            
+            Console.Write("Введите размер массива: ");
+            int len2 = Convert.ToInt32(Console.ReadLine());
+            if (len2 < 2) Console.WriteLine("Нет элементов, стоящих на нечётных позициях");
+            else Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях, составляет: {Summa(len2)}");
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
